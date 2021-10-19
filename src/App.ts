b625a1d3af71ee4cfbe7989bbc589aa55d9ceab7
Wesley Trindade Guarnieri - WGR <wesley.guarnieri@iteris.com.br>
2021-10-19 17:40:51 -0300

    implementação do socket

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
 
 app.use(express.json());
 app.use(router);
@@ -19,10 +35,9 @@ app.get("/signin/callback", (request, response) => {
 });
 
 
-app.use(function(request, response,next){
+app.use(function (request, response, next) {
     response.status(404).end("404: Invalid Route")
 });
 
 
-app.listen(4000, () => console.log("API Rodando na porta 4000"));
-
+export { serverHttp, io }
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { serverHttp } from "./App";
+
+serverHttp.listen(4000, () => console.log("API Rodando na porta 4000"));
